7e4e3d263398ac509d82ed266755601a9f11c73e
Lotte <lottejd@gmail.com>
2023-10-27 20:55:53 +0200

    test done

---
+++ Mini project Note App np/Mini project Note App np/ItemPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Mini_project_Note_App_np
+{
+    public partial class ItemPage : ContentPage
+    {
+        public string headerText { get; set; } = "text";
+        public ItemPage(string item) 
+        {
+            InitializeComponent();
+            headerText = item;
+            Label.Text = headerText;
+        }
+
+        public void back(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
--- Mini project Note App np/Mini project Note App np/MainPage.xaml.cs
+++ Mini project Note App np/Mini project Note App np/MainPage.xaml.cs
@@ -1,24 +1,38 @@
-﻿namespace Mini_project_Note_App_np;
+﻿using static Microsoft.Maui.Controls.Button.ButtonContentLayout;
+using static Microsoft.Maui.Controls.Button;
+
+namespace Mini_project_Note_App_np;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
-	public MainPage()
+    public MainPage()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+        foreach (Button item in stackLayout.Children)
+        {
+            item.BackgroundColor = Color.FromRgb(60, 60, 60);
+        }
+        header.BackgroundColor = Color.FromRgb(60, 60, 60);
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    public void clickBtn(object sender, EventArgs e)
+    {
+        if (sender is Button clickedButton)
+        {
+            Navigation.PushAsync(new ItemPage(clickedButton.Text));
+        }
+        
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    public void Add(object sender, EventArgs e)
+    {
+        Button btn = new Button();
+        btn.Text = "New note";
+        btn.BackgroundColor = Color.FromRgb(60,60,60);
+        btn.ImageSource = "trashbin.png";
+        btn.Clicked += clickBtn;
+        btn.ContentLayout = new ButtonContentLayout(ImagePosition.Right, 300);
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        stackLayout.Children.Add(btn);
+    }
 }
-
